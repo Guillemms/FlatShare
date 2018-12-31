286866439702effd7035c1b140754ed2a8958a3c
PolGerdt <33346265+PolGerdt@users.noreply.github.com>
2018-12-31 01:37:27 +0100

    Agafar tasques de la base de dades i mostrar nom i setmana

--- app/src/main/java/com/example/guillemms/flatshare/TaskListActivity.java
+++ app/src/main/java/com/example/guillemms/flatshare/TaskListActivity.java
@@ -5,16 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
 
 public class TaskListActivity extends AppCompatActivity {
 
-    private ArrayList<Task> tasks;
+    private ArrayList<Map> tasks;
     private RecyclerView taskListRecycler;
+    private int lastDifferentWeekNum = -1;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    DateFormat df = DateFormat.getDateInstance();
+    Calendar cal = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +44,37 @@ public class TaskListActivity extends AppCompatActivity {
 
         tasks = new ArrayList<>();
 
-        tasks.add(new Task("10/11 - 18-11", "Task A week 1"));
-        tasks.add(new Task("10/11 - 18-11", "Task B week 1"));
-        tasks.add(new Task("20/11 - 25-11", "Task A week 2"));
-        tasks.add(new Task("20/11 - 25-11", "Task B week 2"));
-
         taskListRecycler = findViewById(R.id.tasklist_recyclerView);
         taskListRecycler.setLayoutManager(new LinearLayoutManager(this));
-        taskListRecycler.setAdapter(new Adapter());
+
+        final Adapter adapter = new Adapter();
+
+        taskListRecycler.setAdapter(adapter);
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+
+        db.collection("Flats")
+                .document("2UHhup7o3ukp672EwXIo")
+                .collection("Tasks")
+                .orderBy("Initial date")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map flatTask = document.getData();
+                                tasks.add(flatTask);
+                                adapter.notifyItemInserted(tasks.size()-1);
+                            }
+                        } else {
+                            Log.d("test", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -57,9 +101,27 @@ public class TaskListActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            Task task = tasks.get(position);
-            holder.dateView.setText(task.getDate());
-            holder.taskNameView.setText(task.getTaskName());
+            Map task = tasks.get(position);
+            Timestamp initialTimestamp = (Timestamp) task.get("Initial date");
+            Date initialDate = initialTimestamp.toDate();
+
+            cal.setTime(initialDate);
+            int week = cal.get(Calendar.WEEK_OF_YEAR);
+
+            //String initialDateString = df.format(initialDate);
+
+            String taskInfo = task.get("Name").toString();
+
+            holder.dateView.setText("Week: " + week);
+            holder.taskNameView.setText(taskInfo);
+
+            boolean isDateDisplayed = week == lastDifferentWeekNum;
+
+            if(isDateDisplayed) {
+                holder.dateView.setVisibility(View.GONE);
+            } else {
+                lastDifferentWeekNum = week;
+            }
         }
 
         @Override
